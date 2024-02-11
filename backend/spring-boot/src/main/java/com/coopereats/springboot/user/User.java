4e882ead4ef25c9d8b20800486cd7a331418b2c0
justyoyo123 <100324413+justyoyo123@users.noreply.github.com>
2024-02-10 20:47:03 -0500

    renaming users to user

---
+++ backend/spring-boot/src/main/java/com/coopereats/springboot/user/User.java
@@ -0,0 +1,78 @@
+package com.coopereats.springboot.users;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="USERS")
+public class Users {
+
+    @Id
+    @Column(name="USER_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    @Column(name="USER_NAME")
+    private String userName;
+
+    @Column(name="PASSWORD")
+    private String password;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    @Column(name="FULL_NAME")
+    private String fullName;
+
+    @Column(name="PHONE_NUMBER")
+    private String phoneNumber;
+
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
