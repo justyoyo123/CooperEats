4e71a932baa665db470d26fb1f3f46f8ed28281d
justyoyo123 <100324413+justyoyo123@users.noreply.github.com>
2024-02-10 20:47:03 -0500

    renaming users to user

--- backend/spring-boot/src/main/java/com/coopereats/springboot/users/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.coopereats.springboot.users;
-
-import com.coopereats.springboot.users.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
