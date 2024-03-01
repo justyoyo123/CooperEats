b333f16d9a0fdf22b4b37eddb349198816afc403
erictaeyoo <108376780+erictaeyoo@users.noreply.github.com>
2024-02-29 20:28:18 -0500

    Delete backend/menu directory

--- backend/menu/src/main/java/com/example/menu/item/InMemoryItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.menu.item;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InMemoryItemRepository extends CrudRepository<Item, Long> {}
