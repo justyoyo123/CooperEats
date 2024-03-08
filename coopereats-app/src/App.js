2f88c3f0a7ada1cf3fca39b6720e41aa2030da96
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2024-03-08 13:20:34 -0500

    Create CartPage,Header, and Menu Page

--- coopereats-app/src/App.js
+++ coopereats-app/src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-
-import Cart from './components/Cart/Cart';
-
+import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import CartPage from './components/Cart/CartPage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<CartPage />} />
+          {/* Define other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
