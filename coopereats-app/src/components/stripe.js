3f6e9cdce26622ab25127a429c16888508ed7537
justyoyo123 <100324413+justyoyo123@users.noreply.github.com>
2024-03-09 16:55:40 -0500

    remove stripe.js from repo

--- coopereats-app/src/components/stripe.js
+++
@@ -1,5 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('pk_test_51OlKuuBLZS8PieJTSqE97KMAm5iMxnfbLGkaM2WswvxokDh1fKVJrwTP0fbYOUPlUZAa2qwnQe7EdtSmFTd7DMyn0048LJEonR');
-
-export default stripePromise;
