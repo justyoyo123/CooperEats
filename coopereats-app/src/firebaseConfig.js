c29c772c374ec27193ce44f99b9b16ac42496638
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2024-03-09 18:02:42 -0500

    Create firebaseConfig.js

---
+++ coopereats-app/src/firebaseConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHYJpVwOQ-53IZ7pMBq7kLRlPDHHxyxgw",
+    authDomain: "coopereats-92fe8.firebaseapp.com",
+    projectId: "coopereats-92fe8",
+    storageBucket: "coopereats-92fe8.appspot.com",
+    messagingSenderId: "369585848380",
+    appId: "1:369585848380:web:c1827174d2532c43c00339",
+    measurementId: "G-XKFX60P1CB"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  
+  export default firebaseConfig;
