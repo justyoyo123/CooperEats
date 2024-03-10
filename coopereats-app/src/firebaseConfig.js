6ea219e7fb20f4539dd63fda41fe1f35fa8645fb
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2024-03-10 00:34:41 -0500

    Update firebaseConfig.js

--- coopereats-app/src/firebaseConfig.js
+++ coopereats-app/src/firebaseConfig.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCHYJpVwOQ-53IZ7pMBq7kLRlPDHHxyxgw",
     authDomain: "coopereats-92fe8.firebaseapp.com",
