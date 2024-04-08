ce0509240807a8d52b727cb98a29931fbfa64fa8
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2024-04-08 18:29:20 -0400

    Update Header.js

--- coopereats-app/src/components/Header/Header.js
+++ coopereats-app/src/components/Header/Header.js
@@ -2,15 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { Tabs, Tab, Box, Button, IconButton, Snackbar } from '@mui/material';
+import Badge, { BadgeProps } from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import axios from "axios";
 import AdminHeader from './AdminHeader';
 
 function Header() {
   const [user, setUser] = useState(null);
   const [userId, setUserId] = useState(null);
+  const [cart, setCart] = useState([]);
+  const [cartQuantity, setCartQuantity] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false); // State to track if the user is an admin
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleCartUpdate = () => {
+      fetchCartByUserId(); // Assuming this function updates the cart quantity in your header
+    };
+  
+    window.addEventListener('cartUpdated', handleCartUpdate);
+  
+    return () => {
+      window.removeEventListener('cartUpdated', handleCartUpdate);
+    };
+  }, []);
+  
   useEffect(() => {
     const fetchUserId = async (firebaseUid) => {
       try {
@@ -56,6 +78,39 @@ function Header() {
       setUser(currentUser);
     });
   }, []);
+
+  useEffect(() => {
+    if (userId) {
+      fetchCartByUserId();
+      fetchCartQuantity();
+    }
+  }, [userId]); // Fetch cart when userId changes
+
+  // Function to fetch the cart
+  const fetchCartByUserId = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/carts/user/${userId}`);
+      setCart(response.data);
+    } catch (error) {
+      console.error('Failed to fetch cart:', error);
+    }
+  };
+  const fetchCartQuantity = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/carts/user/${userId}`);
+      if (response.data && response.data.products) {
+        // Object.values() is used to get an array of quantities from the products object
+        const totalQuantity = Object.values(response.data.products).reduce((total, qty) => total + qty, 0);
+        setCartQuantity(totalQuantity);
+      } else {
+        setCartQuantity(0);
+      }
+    } catch (error) {
+      console.error('Failed to fetch cart quantity:', error);
+      setCartQuantity(0);
+    }
+  }  
+
   // Here, we decide which header to render based on isAdmin state
   if (isAdmin) {
     return <AdminHeader/>;
@@ -66,25 +121,33 @@ function Header() {
             <img src="./images/design/coopereats_bubble.png" alt="CooperEats Logo" />
           </Link>
           <ul>
-          	<li><Link to="/">Home</Link></li> {/* Re-integrated from HEAD */}
-         	<li><Link to="/food">Food</Link></li>
-          	<li><Link to="/cart">Cart</Link></li>   
+            <li>
+              <IconButton onClick={() => navigate('/food')}><RestaurantMenuIcon /></IconButton>
+            </li>
             {isAdmin && (
                 <li><Link to="/admin">Admin</Link></li> // Admin link, only visible to admins
             )}
             {user ? (
                 <>
-                  <li><Link to="/profile">Profile</Link></li>
-                  <li>{user.email}</li>
                   <li>
-                    <button onClick={() => {
-                      getAuth().signOut().then(() => navigate('/')); // Logout and redirect to '/'
-                    }}>Logout
-                    </button>
+                    <IconButton onClick={() => navigate('/cart')}>
+                      {/* <Badge badgeContent={cartQuantity} color="secondary">
+                        <ShoppingCartIcon color="action" />
+                      </Badge> */}
+                      <ShoppingCartIcon />
+                    </IconButton>
+                  </li>
+                  <li>
+                    <IconButton onClick={() => navigate('/profile')}><AccountCircleIcon /></IconButton>
+                    {/* <Link to="/profile">Profile</Link> */}
+                  </li>
+                  <li>
+                    <IconButton onClick={() => getAuth().signOut().then(() => navigate('/'))}><LogoutIcon /></IconButton>
                   </li>
                 </>
             ) : (
-                <li><Link to="/login">Login</Link></li>
+                <li><IconButton onClick = {() => navigate('/login')}><LoginIcon /></IconButton></li>
+                // <li><Link to="/login">Login</Link></li>
             )}
           </ul>
         </header>
